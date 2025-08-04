284b3c21072fbf3d7a14b51c00998eb92a1bf377
Ashish Kumar <ashishkr45943@gmail.com>
2025-08-04 22:40:47 +0530

    bug fix: token not found

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,27 @@
-// importing the liberies
-import express, { Router } from "express";
 import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
 import cors from "cors";
-// creating a router instance
 import loginRouter from "./routes/auth";
 import contentRouter from "./routes/content";
 
-
-// creating instance of the liberies
 const app = express();
+const port = process.env.PORT;
+
+// Middlewares
+app.use(cors()); 
 app.use(express.json());
-dotenv.config();
-// app.use(cors);
-app.use(cors({
-  origin: 'http://127.0.0.1:5173',  // allow your frontend origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // allowed methods
-}));
 
-// connecting the router
-app.use("/login", loginRouter); 
-app.use("/content", contentRouter);
-
+// Connecting the routers
+app.use("/api/auth", loginRouter); 
+app.use("/api/content", contentRouter);
 
 const JWT_SECRET = process.env.JWT_SECRET;
 if (!JWT_SECRET) {
   throw new Error("JWT_SECRET must be defined in .env file");
 }
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
-
-
